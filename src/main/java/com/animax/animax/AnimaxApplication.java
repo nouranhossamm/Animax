a79933f2addfed082cdea93e64a09220df5d392d
Nouran Hosameldin <nouran_31070288@ics.tanta.edu.eg>
2024-12-31 23:09:23 +0200

    update

---
+++ src/main/java/com/animax/animax/AnimaxApplication.java
@@ -0,0 +1,15 @@
+package com.animax.animax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaxApplication.class, args);
+		System.out.println("hi");
+
+	}
+
+}
